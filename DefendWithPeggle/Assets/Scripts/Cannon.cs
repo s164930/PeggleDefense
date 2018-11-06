ce730fccb8ef2b327cb185f26c53485afb16ee11
Viktor <s164930@student.dtu.dk>
2018-11-06 15:09:40 +0100

    Added kanon, der følger musen

---
+++ DefendWithPeggle/Assets/Scripts/Cannon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+    public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Turn();
+	}
+
+    void Turn()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+}
