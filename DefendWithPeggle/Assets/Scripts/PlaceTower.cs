6a5eb738331354dc92618f8cc863f1e0e40099ed
KenyanJesus <s165249@student.dtu.dk>
2018-11-12 23:50:00 +0100

    update yeET

---
+++ DefendWithPeggle/Assets/Scripts/PlaceTower.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTower : MonoBehaviour {
+
+    public GameObject towerPrefab;
+    private GameObject tower;
+
+    private bool canPlaceTower()
+    {
+        return tower == null;
+    }
+
+    private void OnMouseUp()
+    {
+        if (canPlaceTower())
+        {
+            tower = (GameObject)Instantiate(towerPrefab, transform.position, Quaternion.identity);
+
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}
