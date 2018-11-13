b500f496bb7a2f8ea9e012b183433f2b4e1d3e0a
Viktor <s164930@student.dtu.dk>
2018-11-13 12:44:34 +0100

    Added levelmenu and main menu

---
+++ DefendWithPeggle/Assets/Scripts/ExitGameButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ExitGameButton : MonoBehaviour {
+
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void ExitGameFunction()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.quit();
+#endif
+    }
+}
---
+++ DefendWithPeggle/Assets/Scripts/LevelSelectScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectScript : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
+	}
+}
