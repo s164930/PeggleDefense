3bb0214c2245b7bf53b8e97e821b61f636f56d10
TheAlot <viktorvase@gmail.com>
2018-11-20 14:11:52 +0100

    Added dynamic level menu

---
+++ DefendWithPeggle/Assets/Scripts/LevelButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelButtonScript : MonoBehaviour {
+    public GameObject fader;
+    public void StartLevel()
+    {
+        string levelIndex = this.GetComponentInChildren<Text>().text;
+        fader = GameObject.FindWithTag("FaderTag");
+        fader.GetComponent<SceneFader>().FadeTo("Level_" + levelIndex);
+    }
+}
