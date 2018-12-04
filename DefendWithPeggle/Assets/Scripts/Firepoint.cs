3b5f0dc08803307119d36b46216e23da54fff89e
TheAlot <viktorvase@gmail.com>
2018-12-04 16:51:39 +0100

    Added powerbar to cannon barrel, it follows the barrel in a wierd way

--- DefendWithPeggle/Assets/Scripts/Firepoint.cs
+++ DefendWithPeggle/Assets/Scripts/Firepoint.cs
@@ -1,28 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Firepoint : MonoBehaviour {
-    public int force = 10;
-    public int damage = 10;
+    public float force = 0.0f;
     public GameObject ball;
+    public float forceIncrease = 5f;
+    public Image process;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
+    private bool isPressed = false;
 	// Update is called once per frame
 	void Update () {
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        direction = direction.normalized;
 
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButtonDown(0) && !isPressed)
         {
+            isPressed = true;
+        }
+        if (Input.GetMouseButtonUp(0)){
+            isPressed = false;
             GameObject newBullet = Instantiate(ball, transform.position, transform.rotation);
             Rigidbody2D rigid = newBullet.GetComponent<Rigidbody2D>();
+            
             rigid.AddForce(direction * force);
+            force = 0;
+            process.fillAmount = 0f;
         }
 
+        if (isPressed) {
+            process.fillAmount = force / 1000f;
+            force += forceIncrease;
+            if (force > 1000f)
+            {
+                force = 1000f;
+            }
+        }
 
 
     }
---
+++ DefendWithPeggle/Assets/Scripts/PowerBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBar : MonoBehaviour {
+    public GameObject barrel;
+    private readonly float distanceToBarrely = 2.5f;
+    private readonly float distanceToBarrelx = 0.25f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = barrel.transform.TransformPoint(distanceToBarrelx, distanceToBarrely, 0);
+        transform.rotation = barrel.transform.rotation;
+	}
+}
